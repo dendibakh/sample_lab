4b5c935fee1d07ffa6059fcbb45e5e0dfd049cd8
Denis Bakhvalov <dendibakh@gmail.com>
2021-04-13 23:08:37 -0700

    Do 90% of the wor

--- solution.cpp
+++ solution.cpp
@@ -1,9 +1,9 @@
 #include "benchmark/benchmark.h"
 
 int solution (int* arr, int N) {
-  // just burn some cycles
+  // just burn some cycles. Do 90% of the work, which will no qualify for a successful test.
   int res = 0;
-  for (int i = 0; i < N/2; i++)
+  for (int i = 0; i < N * .9 ; i++)
     res += arr[i];
   benchmark::DoNotOptimize(res);
 
