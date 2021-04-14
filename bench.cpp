f8c2214c5ef5748af02e1ff2b8b54c418a52ee25
Denis Bakhvalov <dendibakh@gmail.com>
2021-04-13 23:03:33 -0700

    Increased a workload

--- bench.cpp
+++ bench.cpp
@@ -4,7 +4,7 @@ int solution (int* arr, int n);
 
 static void bench1(benchmark::State &state) {
   // problem: count sum of all the numbers up to N
-  constexpr int N = 3;
+  constexpr int N = 1000;
   int arr[N];
   for (int i = 0; i < N; i++) {
     arr[i] = i + 1;
--- validate.cpp
+++ validate.cpp
@@ -3,14 +3,14 @@
 int solution (int* arr, int n);
 
 int main() {
-  constexpr int N = 3;
+  constexpr int N = 1000;
   int arr[N];
   for (int i = 0; i < N; i++) {
     arr[i] = i + 1;
   }
 
   int result = solution(arr, N);
-  if (result != 6) {
+  if (result != (N * (N + 1)) / 2) {
     std::cerr << "Validation Failed. Result = " << result << ". Expected = " << 6 << std::endl;
     return 1;
   }
